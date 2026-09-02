18a0b64fd7a7d755769ec3f0c45eae060bf531ca
hamiltonr17 <rosaleshamilton8@gmail.com>
2026-09-02 13:58:42 +0000

    WIP Platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,24 +33,32 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
 
+    // createPlatform(x, y, long, tall, color);
 
-
+    createPlatform(55, 150, 150, 55, "lime");
+    createPlatform(55, 97, 73, 1, "cyan");
+    createPlatform(170, 50, 10, 100, "blue");
+    createPlatform(160, 150, 56, 50, "lime");
+    createPlatform();
+    createBadPlatform(0, 700, 1400, 0.1);
+    createPlatform(50,349,40,10,"pink")
+    createPlatform(5,260,4,500,"cyan")
+    createPlatform(110,260,1,350,"cyan")
+    createPlatform(50,450,40,10,"pink")
 
     // TODO 3 - Create Collectables
+    createCollectable("excel",115.5,50)
+    createCollectable("word",500,500)
 
-
-
-    
     // TODO 4 - Create Cannons
 
+    createCannon("top", 200, 300);
+    createCannon("left", 196, 4000);
 
-    
-    
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -3,7 +3,7 @@ const walkAcceleration = 2.5; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
 const friction = 1.5; // how much the player is slowed each frame
 const maxSpeed = 8; // maximum horizontal speed, not vertical
-const playerJumpStrength = 12; // this is subtracted from the speedY each jump
+const playerJumpStrength = 7.3; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 let shouldDrawGrid = false;
 let gridMade = false;
@@ -96,10 +96,11 @@ let halleImage;
 let animationDetails = {};
 
 var collectableList = {
-  database: { image: "images/collectables/database.png" },
-  diamond: { image: "images/collectables/diamond-head.png" },
+  excel: { image: "images/collectables/database.png" },
   grace: { image: "images/collectables/grace-head.png" },
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  excel: {image: "images/collectables/excel.png"},
+  word: {image: "images/collectables/word.png"}
 };
